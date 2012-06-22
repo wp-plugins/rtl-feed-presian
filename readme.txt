2205aeffdcffa7ede2fc56e36282542f14251466
behrouzpc <behrouzpc@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-22 19:27:50 +0000

    git-svn-id: https://plugins.svn.wordpress.org/rtl-feed-presian/trunk@562637 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,25 @@
+=== Plugin Name ===
+Contributors: behrouzpc
+Donate link: http://www.iflashlord.com/
+Tags: rtl, feed , presian , farsi , iflashlord
+Requires at least: 2.0.2
+Tested up to: 3.4
+Stable tag: 4.3
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+  Make Wordpress Feed RTL and Tahoma Font 
+== Description ==
+
+ Writing and view correct font (Tahoma) problem RTL, the Persian language, for WordPress feeds. 
+
+== Installation ==
+
+1. Upload `rtlFeedPresian.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Changelog ==
+
+= 1.0 =
+Initial version.
+
---
+++ rtlFeedPresian.php
@@ -0,0 +1,40 @@
+<?php
+/*
+
+Plugin Name: RTL-Feed-Presian
+
+Plugin URI: http://blog.iflashlord.com/?p=2080
+
+Description: Writing and view correct font (Tahoma) problem RTL, the Persian language, for WordPress feeds. 
+
+Version: 1.0
+
+Author: Behrouz Pooladrag
+Author URI: http://www.iflashlord.com
+
+License: GPL
+
+*/
+
+
+function ifl_rtl_font_for_feeds( $content ) {
+
+	global $post, $id;
+	$blog_key = substr( md5( get_bloginfo('url') ), 0, 16 );
+	if ( ! is_feed() ) return $content;
+	
+	// Displaying the content ***
+	$content = str_replace('<p>','<p style="font-family: Tahoma,Verdana,sans-serif; direction: rtl;" align="right">' ,$content); 
+	$content = str_replace('<span>','<span style="font-family: Tahoma,Verdana,sans-serif; direction: rtl;" align="right">' ,$content); 
+	$content = str_replace('<ul>','<ul style="font-family: Tahoma,Verdana,sans-serif; direction: rtl;" align="right">' ,$content); 
+	$content = str_replace('<ol>','<ol style="font-family: Tahoma,Verdana,sans-serif; direction: rtl;" align="right">' ,$content); 
+	$content = str_replace('<div>','<div style="font-family: Tahoma,Verdana,sans-serif; direction: rtl;" align="right">' ,$content); 
+	$content = str_replace('<p dir="rtl">','<p style="font-family: Tahoma,Verdana,sans-serif; direction: rtl;" align="right" dir="rtl">' ,$content); 
+	
+		return $content;
+		
+} // End function
+add_filter('the_content', 'ifl_rtl_font_for_feeds');
+
+
+?>
